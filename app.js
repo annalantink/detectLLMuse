3a74a78f113b9732c03c479b85cb40a069fd360d
Anna Natalie Lantink <A.N.Lantink@student.tudelft.nl>
2025-12-12 10:47:34 +0100

    Create summarization task pages

--- app.js
+++ app.js
@@ -7,9 +7,21 @@ const port = 3000
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {
-  res.sendFile("index.html", { root: "/public" });
+  res.sendFile("index.html", { root: __dirname + "/public" });
+})
+
+app.get('/example_summary', (req, res) => {
+  res.sendFile("example_summary.html", { root: __dirname + "/public" });
+})
+
+app.get('/summarize_one', (req, res) => {
+  res.sendFile("summarize_one.html", { root: __dirname + "/public" });
+})
+
+app.get('/summarize_two', (req, res) => {
+  res.sendFile("summarize_two.html", { root: __dirname + "/public" });
 })
 
 app.listen(port, () => {
-  console.log(`Crowd computing application running on port ${port}`)
+  console.log("Crowd computing application running on port ${port}")
 })
