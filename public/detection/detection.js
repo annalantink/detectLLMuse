bc0c6989b275591268c827024e5b3189d9be4579
Sahar Marossi <s.marossi@student.tudelft.nl>
2025-12-23 21:33:08 +0100

    Moved pop-up logic to detection.js

--- public/detection/detection.js
+++ public/detection/detection.js
@@ -1,11 +1,36 @@
-// function showPopup(reason) {
-//   console.log("Triggering popup due to:", reason);
-//   document.getElementById("detectionModal").style.display = "block";
-// }
+// === POP-UP MANAGER ===
+function showPopup(reason) {
+  if (WPM_MODAL_OPEN) return; // pop-up flag already active!
+  WPM_MODAL_OPEN = true;
 
-// function closePopup() {
-//   document.getElementById("detectionModal").style.display = "none";
-// }
+  console.log("Triggering popup due to:", reason);
+
+  // defocus the text-area
+  try {
+    var active = document.activeElement;
+    if (active && active.classList && active.classList.contains("input_summary")) {
+      window.WPM_DETECTOR._prevFocus = active;
+      active.blur();
+    }
+  } catch (e) {}
+
+  document.getElementById("detectionModal").style.display = "block";
+
+  // pause all detectors
+  window.WPM_DETECTOR.pauseAll();
+}
+
+function closePopup() {
+  document.getElementById("detectionModal").style.display = "none";
+  WPM_MODAL_OPEN = false; // release flag
+  console.log("Pop-up closed, resuming detection.");
+
+  // reset all metrics when closing pop-up
+  window.WPM_DETECTOR.resetAll();
+
+  // unpause all detectors
+  window.WPM_DETECTOR.resumeAll();
+}
 
 function setupDetection(textareaId, taskName) {
   const textarea = document.getElementById(textareaId);
