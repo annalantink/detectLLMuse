2162bec8bb82637fd8f49cc8a3805ee63c3aadca
Gabrium0 <gabriegamepass@hotmail.com>
2026-01-09 12:26:02 +0100

    Detect tab switch within each page.

--- public/detection/behavior/tabSwitch.js
+++ public/detection/behavior/tabSwitch.js
@@ -1,9 +1,12 @@
 // Eventlistener for tab-switching
-document.addEventListener("visibilitychange", (event) => {
-  if (document.visibilityState == "visible") {
-  } else {
-    let currentCount = parseInt(sessionStorage.getItem('tab_switch')) || 0;
-    sessionStorage.setItem('tab_switch', currentCount + 1);
-    console.log(parseInt(sessionStorage.getItem('tab_switch')) || 0);
-  }
-});
+
+function detectTabSwitch(number) {
+  document.addEventListener("visibilitychange", (event) => {
+    if (document.visibilityState == "visible") {
+    } else {
+      let currentCount = parseInt(sessionStorage.getItem(`tabswitchCount${number}`)) || 0;
+      sessionStorage.setItem(`tabswitchCount${number}`, currentCount + 1);
+      console.log(parseInt(sessionStorage.getItem(`tabswitchCount${number}`)) || 0);
+    }
+  });
+}
