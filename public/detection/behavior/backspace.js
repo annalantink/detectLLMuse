3fc8d05d95c254a9e9394070dd16370c6a92f96d
Anna Natalie Lantink <A.N.Lantink@student.tudelft.nl>
2026-01-15 13:03:05 +0100

    Add comments to the code

--- public/detection/behavior/backspace.js
+++ public/detection/behavior/backspace.js
@@ -1,10 +1,14 @@
+// Function to detect the number of backspaces hit while typing a summary
 function backspaceDetector(el) {
     var numKeystrokes = 0
     var numBackspaces = 0
 
+    // Event listener that tracks keystrokes
     el.addEventListener("keydown", function (e) {
         numKeystrokes += 1;
         if (e.code == "Backspace") numBackspaces += 1;
+
+        // If the number of keystrokes is sufficiently high and the proportion of backspaces is less than 2% -> activate pop-up
         if (numKeystrokes >= 150 && numBackspaces / numKeystrokes < 0.02) {
             numBackspaces = 0;
             numKeystrokes = 0;
